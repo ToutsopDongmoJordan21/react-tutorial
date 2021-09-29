3c1f66f1e01b81b8ca0334cacc372e2eb664b91c
ToutsopDongmoJordan21 <toutsopjordan1999@gmail.com>
2021-09-29 16:30:15 +0100

    deuxiéme commit

--- src/index.js
+++ src/index.js
@@ -4,21 +4,58 @@ import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+class App extends React.Component {
+  state = {
+    clients: [
+      {id: 1, nom: "Toutsop Jordan"},
+      {id: 2, nom: "Yvan barrela"},
+      {id: 3, nom: "Mauricio Hilton"},
+      {id: 4, nom: "Armand Lamela"},
+    ],
+  };
+
+
+  HandlerClick = () => {
+    const client = this.state.clients.slice();
+    client.push({id: 5, nom: "toto"});
+    
+    this.setState({clients: client});
+  }
+
+  HandleDelete = id => {
+    const clients = this.state.clients.slice();
+    const index = clients.findIndex(client => client.id === id);
+
+    clients.splice(index, 1);
+
+    this.setState({clients: clients});
+    console.log('le id supprimé es:' +id)
+  };
+
+  render() {
+    const title = "Liste des client";
+    return (
+      <div>
+        <h1>{title} </h1>
+        <ul>
+          {this.state.clients.map(client => (
+            <li>{client.nom} <button onClick={() => this.HandleDelete(client.id)}>X</button> 
+                              <button onClick={this.HandlerClick}>+</button></li>
+              ))}
+        </ul>
+        <form>
+          <input type="text" placeholder="Ajouter un client"/>
+          <button>Confirmer</button>
+        </form>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-
   <React.StrictMode>
-
-    <h1>Liste des clients </h1>
-      <ul>
-        <li>Toutsop Jordan <button>X</button></li>
-        <li>Alongo Daniel <button>X</button></li>
-        <li>William Wamba <button>X</button></li>
-        <li>Epasso Jean <button>X</button></li>
-      </ul>
-      <form>
-        <input type="text" placeholder="Ajouter client"/>
-        <button>Confirmer</button>
-      </form>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
