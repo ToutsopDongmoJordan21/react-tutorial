aa59303509c5af76d09d92be9982bb3354558a18
ToutsopDongmoJordan21 <toutsopjordan1999@gmail.com>
2021-09-30 14:37:00 +0100

    la gestion du formulaire

--- src/Client.js
+++ src/Client.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 
 class Client extends Component {
     render() {
+        // const details = this.props.details;
+        // const onDelete = this.props.onDelete;
+        // const onAdd = this.props.onAdd;
+
+        const {details, onDelete, onAdd } = this.props;
         return <li>
-                {this.props.details.nom} <button onClick={() => this.props.onDelete(this.props.details.id)}>X</button> 
-                          <button onClick={this.props.onAdd}>+</button></li>
+                {details.nom} <button onClick={() => onDelete(details.id)}>X</button> 
+                          <button onClick={onAdd}>+</button></li>
     }
 }
 
